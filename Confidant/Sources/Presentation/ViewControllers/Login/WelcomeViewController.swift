245ea255acff3db5361c7d7ae3de808780ff374d
Michael Douglas <michael.dcsouza@gmail.com>
2017-03-07 02:13:58 -0300

    - fix scroll message and change page

--- Confidant/Sources/Presentation/ViewControllers/Login/WelcomeViewController.swift
+++ Confidant/Sources/Presentation/ViewControllers/Login/WelcomeViewController.swift
@@ -49,8 +49,8 @@ fileprivate struct WelcomeMessages {
 //
 //**************************************************************************************************
 
-class WelcomeViewController: UIViewController {
-
+class WelcomeViewController: UIViewController, UIScrollViewDelegate {
+    
     //*************************************************
     // MARK: - IBOutlets
     //*************************************************
@@ -78,10 +78,6 @@ class WelcomeViewController: UIViewController {
     // MARK: - Properties
     //*************************************************
     
-    private var messageView: UIView!
-    private var messageTitle: UILabel!
-    private var messageText: UILabel!
-    
     //*************************************************
     // MARK: - Override Public Methods
     //*************************************************
@@ -89,6 +85,36 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadWelcomeMessages()
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeRecognizer(_:)))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeRecognizer(_:)))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+    
+    //*************************************************
+    // MARK: - Public Methods
+    //*************************************************
+    
+    internal func swipeRecognizer(_ gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.right:
+            if self.messagePageControl.currentPage != 4 {
+                self.messageScrollView.setContentOffset(CGPoint(x: self.messageScrollView.contentOffset.x - self.messageScrollView.frame.size.width, y: 0), animated: true)
+            } else {
+                self.messageScrollView.setContentOffset(CGPoint(x: self.messageScrollView.contentOffset.x - self.messageScrollView.frame.size.width, y: 0), animated: true)
+            }
+        case UISwipeGestureRecognizerDirection.left:
+            print("left")
+            if self.messagePageControl.currentPage != 4 {
+                self.messageScrollView.setContentOffset(CGPoint(x: self.messageScrollView.contentOffset.x + self.messageScrollView.frame.size.width, y: 0), animated: true)
+            } else {
+                self.messageScrollView.setContentOffset(CGPoint(x: self.messageScrollView.contentOffset.x - (self.messageScrollView.frame.size.width * 4), y: 0), animated: false)
+            }
+        default: break
+        }
     }
     
     //*************************************************
@@ -97,50 +123,72 @@ class WelcomeViewController: UIViewController {
     
     private func loadWelcomeMessages() {
         
-        //Setup Size and Position MessageView, Title and Text
-        self.messageView = UIView(frame: CGRect(x: 0, y: 0, width: CGFloat(self.messageScrollView.bounds.size.width), height: CGFloat(self.messageScrollView.bounds.size.height)))
-        self.messageTitle = UILabel(frame: CGRect(x: CGFloat(self.messageView.frame.size.width - 345) / 2, y: 0, width: 345, height: 20))
-        self.messageText = UILabel(frame: CGRect(x: CGFloat(self.messageView.frame.size.width - 345) / 2, y: CGFloat(self.messageTitle.frame.size.height + 3), width: 345, height: 45))
+        let welcomeMessages = WelcomeMessages().getMessages()
         
-        //Configure MessageView Background
-        self.messageView.backgroundColor = UIColor.clear
+        for (index, message) in welcomeMessages.enumerated() {
+            
+            self.messageScrollView.contentSize = CGSize(width: self.view.bounds.size.width * CGFloat(welcomeMessages.count), height: self.messageScrollView.frame.size.height)
+            
+            //Setup Size and Position MessageView, Title and Text
+            let messageView = UIView(frame: CGRect(x: 0, y: 0, width: CGFloat(self.messageScrollView.bounds.size.width), height: CGFloat(self.messageScrollView.bounds.size.height)))
+            let messageTitle = UILabel(frame: CGRect(x: CGFloat(messageView.frame.size.width - 345) / 2, y: 0, width: 345, height: 20))
+            let messageText = UILabel(frame: CGRect(x: CGFloat(messageView.frame.size.width - 345) / 2, y: CGFloat(messageTitle.frame.size.height + 3), width: 345, height: 45))
+            
+            //Configure MessageView Background
+            messageView.backgroundColor = UIColor.clear
+            
+            //Configure MessageTitle and MessageText to input in MessageView
+            if let title = message["title"] {
+                messageTitle.getMessageTitleFormat(title: title)
+            }
+            if let text = message["text"] {
+                messageText.getMessageTextFormat(text: text)
+            }
+            
+            //Input MessagesLabels into MessageView
+            messageView.addSubview(messageTitle)
+            messageView.addSubview(messageText)
+            
+            messageView.frame.origin.x = CGFloat(index) * self.messageScrollView.frame.size.width
+            
+            //Input MessageView into MessageScrollView
+            self.messageScrollView.addSubview(messageView)
+            
+        }
         
-        //Configure MessageTitle and MessageText to input in MessageView
-        var attributedString = NSMutableAttributedString(string: "Welcome")
-        attributedString.addAttribute(NSKernAttributeName, value: -0.5, range: NSRange(location: 0, length: attributedString.length))
-        self.messageTitle.attributedText = attributedString
-        self.messageTitle.font = UIFont(name: "Gotham-Bold", size: 22)
-        self.messageTitle.textColor = UIColor.white
-        self.messageTitle.textAlignment = .center
-        
-        attributedString = NSMutableAttributedString(string: "Feel free for help someone or keep someone help you, find your Confidant.")
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = self.messageText.textAlignment
-        paragraphStyle.lineSpacing = 10
-        attributedString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
-        self.messageText.attributedText = attributedString
-        self.messageText.font = UIFont(name: "Gotham-Bold", size: 12)
-        self.messageText.numberOfLines = 0
-        self.messageText.textColor = UIColor(white: 100, alpha: 0.7)
-        self.messageText.textAlignment = .center
-        
-        self.messageView.addSubview(self.messageTitle)
-        self.messageView.addSubview(self.messageText)
-        
-        print(self.messageTitle.frame)
-        print(self.messageText.frame)
-        
-        //Configure MessagesLabel to input in MessageView
-        
-        
-        
-        self.messageScrollView.addSubview(self.messageView)
-//        
-//        let messages = WelcomeMessages().getMessages()
-//        for (index, message) in messages.enumerated() {
-//            
-//        }
     }
     
+    internal func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.size.width
+        self.messagePageControl.currentPage = Int(page)
+    }
+    
+}
 
+//**************************************************************************************************
+//
+// MARK: - Extension - UILabel - WelcomeMessageFormat
+//
+//**************************************************************************************************
+
+extension UILabel {
+    func getMessageTitleFormat(title: String) {
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(NSKernAttributeName, value: -0.5, range: NSRange(location: 0, length: attributedString.length))
+        self.attributedText = attributedString
+        self.font = UIFont(name: "Gotham-Bold", size: 22)
+        self.textColor = UIColor.white
+        self.textAlignment = .center
+    }
+    func getMessageTextFormat(text: String) {
+        let attributedString = NSMutableAttributedString(string: text)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+        attributedString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+        self.attributedText = attributedString
+        self.font = UIFont(name: "Gotham-Bold", size: 12)
+        self.textColor = UIColor(white: 100, alpha: 0.7)
+        self.textAlignment = .center
+        self.numberOfLines = 0
+    }
 }
