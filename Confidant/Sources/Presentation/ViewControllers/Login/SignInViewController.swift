a387c65e6271aeb4c4b2d7850eb4e57c005173e6
Michael Douglas <michael.dcsouza@gmail.com>
2017-03-10 17:18:04 -0300

    - fix LogInViewController class to storyboard

--- Confidant/Sources/Presentation/ViewControllers/Login/SignInViewController.swift
+++ Confidant/Sources/Presentation/ViewControllers/Login/SignInViewController.swift
@@ -37,10 +37,5 @@ class SignInViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 }
